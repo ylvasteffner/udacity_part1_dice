d6e382575c76a75ebe779db9110fc35cf8b82768
Ylva Steffner <ylva.steffner@gmail.com>
2022-03-04 14:16:01 +0100

    adding files

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView // The views of the layout are not accessible before they've
+                                    // been inflated be setContentView, therefore we can't initialise
+                                    // it here. We would have to set it as ?= null - which would have
+                                    // complecated our code a lot checking for null so instead we can
+                                    // use lateinit which means we promise the compiler we will initialise
+                                    // this variable before calling any operations on it
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+        diceImage = findViewById(R.id.dice_image)
+    }
+
+    private fun rollDice() {
+        val randomInt = java.util.Random().nextInt(6)+1
+        val drawableResource = when (randomInt){
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+
+}
